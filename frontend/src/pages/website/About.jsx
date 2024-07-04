a9caadf96bcb39322248cfce040d4ce57b5875a1
Rahul <rahul.yadav@valuepitch.com>
2024-07-04 17:35:33 +0530

    Frontend - Updated About Page

--- frontend/src/pages/website/About.jsx
+++ frontend/src/pages/website/About.jsx
@@ -1,111 +1,158 @@
-import { Breadcrumb } from "../../components";
-import { AboutImg } from "../../static/images/about";
+import {
+  AboutImg,
+  WhyChooseUsImg,
+  Icon01,
+  Icon02,
+  Icon03,
+} from "../../static/images/about";
 
 const About = () => {
   return (
     <>
-      {/* Breadcrumb */}
-      <Breadcrumb />
-
-      {/* About Page Us */}
-      <section className="section about">
+      {/* Hero Section */}
+      <section className="section hero about-hero pb-8">
         <div className="container">
-          <div className="grid grid-cols-2">
-            <div className="col-span-2">
-              <h5 className="title mb-4">
-                Unleash Your Career Potential with TheResume!
+          <div className="text">
+            <h5>ABOUT US</h5>
+            <h4>Unleash Your Career Potential With TheResume!</h4>
+            <p className="mb-8">
+              Welcome to TheResume, where we specialize in transforming your
+              career aspirations into compelling resumes that stand out. At
+              TheResume, we understand that your resume is more than just a
+              document; it's your professional story, your gateway to new
+              opportunities.
+            </p>
+          </div>
+          <div className="image">
+            <img src={AboutImg} alt="Resume" />
+          </div>
+        </div>
+      </section>
+
+      {/* About Section */}
+      <section className="section about-text pb-0">
+        <div className="container">
+          <div className="grid grid-cols-2 gap-10">
+            <div>
+              <h5 className="title-alt mb-4">
+                Ready To Ditch The Resume Blues and Land Your Dream Job?
               </h5>
-              <p className="mb-8">
-                Welcome to TheResume, where we specialize in transforming your
-                career aspirations into compelling resumes that stand out. At
-                TheResume, we understand that your resume is more than just a
-                document; it's your professional story, your gateway to new
-                opportunities.
-              </p>
-              <h6 className="title-alt mb-3">
-                Ready to ditch the resume blues and land your dream job?
-              </h6>
-              <p className="mb-8">
-                <b>TheResume</b> is here to turn your career aspirations into
-                resume magic that gets you noticed!
-              </p>
-              <h6 className="title-alt mb-3">
-                We don't just help write resumes, we craft powerful career
-                stories.
-              </h6>
-              <p className="mb-8">
-                Your resume is your <b>golden ticket</b> to new opportunities,
-                and we'll make sure it shines brighter than the competition.
+              <p>
+                TheResume is here to turn your career aspirations into resume
+                magic that gets you noticed!
               </p>
             </div>
+            <div>
+              <h5 className="title-alt mb-4">
+                We Don't Just Help Write Resumes, We Craft Powerful Career
+                Stories.
+              </h5>
+              <p>
+                Your resume is your golden ticket to new opportunities, and
+                we'll make sure it shines brighter than the competition.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
 
-            <div className="col flex items-center">
-              <div className="image">
-                <img src={AboutImg} alt="" />
+      {/* CTA Section */}
+      <section className="section cta about-choose bg-light pt-0">
+        <div className="image">
+          <img src={WhyChooseUsImg} alt="Why Choose Us" />
+        </div>
+        <div className="container">
+          <div className="text">
+            <h4 className="title mb-8">Why Choose TheResume?</h4>
+            <ul>
+              <li>
+                <b>Stand Out From the Crowd:</b>
+                <span>
+                  Our expert resume writers are resume rockstars! They'll help
+                  you create a resume that stands out, no matter where you're
+                  applying.
+                </span>
+              </li>
+              <li>
+                <b>Personalized for You:</b>
+                <span>
+                  Every career journey is one-of-a-kind. Whether you're a
+                  seasoned pro ready to conquer the next level or a fresh
+                  graduate embarking on your professional odyssey, we'll help
+                  you tailor your resume to showcase your unique skills and
+                  achievements.
+                </span>
+              </li>
+              <li>
+                <b>Expertise You Can Trust: </b>
+                <span>
+                  We're resume ninjas with years of experience and a finger on
+                  the pulse of the latest hiring trends. Your resume will be a
+                  masterpiece that grabs the attention of recruiters and hiring
+                  managers.
+                </span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+
+      {/* About Icons */}
+      <section className="section about-icons">
+        <div className="container">
+          <div className="grid grid-cols-3 gap-20">
+            <div className="col">
+              <div className="about-item">
+                <div className="head">
+                  <div className="icon">
+                    <img src={Icon01} alt="" />
+                  </div>
+                  <h5>Your Success Is Our Passion!</h5>
+                </div>
+                <div className="body">
+                  <p>
+                    At TheResume, we're obsessed with your success. We don't
+                    settle for "good enough" - we create resumes that exceed
+                    your expectations and open doors to exciting possibilities.
+                  </p>
+                </div>
               </div>
             </div>
-
-            <div className="col flex items-center">
-              <div className="text">
-                <h5 className="title-alt mb-8">Why Choose TheResume?</h5>
-                <ul>
-                  <li>
-                    <b>Stand Out From the Crowd:</b>
-                    <span>
-                      Our expert resume writers are resume rockstars! They'll
-                      tailor your resume to showcase your unique awesomeness and
-                      achievements, making you an irresistible candidate.
-                    </span>
-                  </li>
-                  <li>
-                    <b>Personalized for You:</b>
-                    <span>
-                      Every career journey is one-of-a-kind. Whether you're a
-                      seasoned pro ready to conquer the next level or a fresh
-                      graduate embarking on your professional odyssey, we'll
-                      work closely with you to craft a resume that screams
-                      success.
-                    </span>
-                  </li>
-                  <li>
-                    <b>Expertise You Can Trust:</b>
-                    <span>
-                      We're resume ninjas with years of experience and a finger
-                      on the pulse of the latest hiring trends. Your resume will
-                      be a masterpiece that grabs the attention of recruiters
-                      and hiring managers.
-                    </span>
-                  </li>
-                </ul>
+            <div className="col">
+              <div className="about-item">
+                <div className="head">
+                  <div className="icon">
+                    <img src={Icon02} alt="" />
+                  </div>
+                  <h5>Ready To Launch Your Career Takeoff?</h5>
+                </div>
+                <div className="body">
+                  <p>
+                    Let TheResume be your co-pilot! We'll craft a resume that
+                    gets you noticed and positions you for the success you
+                    deserve. Join countless others who have transformed their
+                    careers with TheResume!
+                  </p>
+                </div>
               </div>
             </div>
-
-            <div className="col-span-2">
-              <h5 className="title-alt mb-4">Your Success is Our Passion!</h5>
-              <p className="mb-8">
-                At TheResume, we're obsessed with your success. We don't settle
-                for "good enough" - we create resumes that{" "}
-                <b>exceed your expectations</b> and open doors to exciting
-                possibilities.
-              </p>
-
-              <h5 className="title-alt mb-4">
-                Ready to Launch Your Career Takeoff?
-              </h5>
-              <p className="mb-8">
-                Let TheResume be your co-pilot! We'll craft a resume that gets
-                you noticed and positions you for the success you deserve. Join
-                countless others who have transformed their careers with
-                TheResume!
-              </p>
-
-              <h5 className="title-alt mb-4">
-                Craft Your Dream Job. Build Your Future. It Starts Here.
-              </h5>
-              <p className="sub-heading">
-                Don't wait! Transform your career trajectory today with
-                TheResume - where your professional journey takes flight!
-              </p>
+            <div className="col">
+              <div className="about-item">
+                <div className="head">
+                  <div className="icon">
+                    <img src={Icon03} alt="" />
+                  </div>
+                  <h5>
+                    Craft Your Dream Job. Build Your Future. It Starts Here.
+                  </h5>
+                </div>
+                <div className="body">
+                  <p>
+                    Don't Wait! Transform Your Career Trajectory Today With
+                    TheResume - Where Your Professional Journey Takes Flight!
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
--- frontend/src/static/images/about/index.js
+++ frontend/src/static/images/about/index.js
@@ -1,3 +1,7 @@
-import AboutImg from "./about.jpg";
+import AboutImg from "./about.png";
+import WhyChooseUsImg from "./why-choose.png";
+import Icon01 from "./icon-01.png";
+import Icon02 from "./icon-02.png";
+import Icon03 from "./icon-03.png";
 
-export { AboutImg };
+export { AboutImg, WhyChooseUsImg, Icon01, Icon02, Icon03 };
